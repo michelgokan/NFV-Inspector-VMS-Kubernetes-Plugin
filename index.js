5dbb4fe66ecb07f2e3b1042580cc21ae856dfc8f
Michel Gokan <michel.gokan@kau.se>
2018-10-29 22:45:36 +0100

    Adding models

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-module.exports = function (loopbackApplication, options) {
-    loopbackApplication.use(options.path, function (req, res, next) {
-        res.send('Your Component');
-    });
-};
+'use strict';
+
+var path = require('path');
+var SG = require('strong-globalize');
+SG.SetRootDir(path.join(__dirname, '..'));
+
+var loopback = require('loopback');
+var DataModel = loopback.PersistedModel || loopback.DataModel;
+
+function loadModel(jsonFile) {
+    var modelDefinition = require(jsonFile);
+    return DataModel.extend(modelDefinition.name,
+        modelDefinition.properties,
+        {
+            relations: modelDefinition.relations,
+        });
+}
+
+var NodeTypeModel = loadModel('./models/node-type.json');
+
+exports.NodeType = require('./models/node-type')(NodeTypeModel);
+
+
+exports.NodeType.autoAttach = 'db';
+
+//exports.NFVInspector.NFVVMS.Configuration = require('./passport-configurator');
